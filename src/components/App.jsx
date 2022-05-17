f363cbc6a53a24919728dd56012dcb9596505809
Alona Puhach <alen1414@gmail.com>
2022-05-17 14:29:05 -0400

    functiobs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,72 @@
-import Feedback from 'components/Feedback/Feedback';
+import React from 'react';
+/* import shortid from 'shortid'; */
+import 'components/Feedback/feedback.css';
+import Section from 'components/Feedback/Section';
+import Statistics from 'components/Feedback/Statistics';
+import Notification from 'components/Feedback/Notoficaion';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  hendelGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  hendelNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  hendelBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  totalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  totalPositivFeedback = () => {
+    return Math.round((this.state.good / this.totalFeedback()) * 100);
+  };
+  render() {
+    const total = this.totalFeedback();
+    const positiv = this.totalPositivFeedback();
+
+    return (
+      <div className="Feedback">
+        <h1> Please leave feedback!</h1>
+
+        <button type="button" onClick={this.hendelGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.hendelNeutral}>
+          Neutral
+        </button>
+
+        <button type="button" onClick={this.hendelBad}>
+          Bad
+        </button>
+
+        <Section title="Statistics">
+          {total < 1 ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={total}
+              positiv={positiv}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
